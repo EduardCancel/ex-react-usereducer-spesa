6f1c817022135bd2dae10dce7029ecf2efda4b10
IlTuoNome <laTuaEmail@example.com>
2025-07-11 13:12:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+function App() {
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
+  return (
+    <>
+      <h1>Lista della Spesa</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - €{product.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
