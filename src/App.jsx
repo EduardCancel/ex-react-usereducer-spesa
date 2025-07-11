fe9fe1c799823344876a894561a1041a5fc5ddc8
IlTuoNome <laTuaEmail@example.com>
2025-07-11 13:30:32 +0200

    add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import { useState } from "react";
+import './App.css';
 
 function App() {
   const products = [
@@ -8,16 +9,48 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProduct, setAddedProduct] = useState([]);
+
+  const addToCart = product => {
+    const isProductAlreadyAdded = addedProduct.some(p => p.name === product.name);
+    if (isProductAlreadyAdded) {
+      return;
+    }
+
+    const productToAdd = {
+      ...product,
+      quantity: 1,
+    }
+
+    setAddedProduct(curr => [...curr, productToAdd]);
+  }
+
+
   return (
     <>
       <h1>Lista della Spesa</h1>
       <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            {product.name} - €{product.price.toFixed(2)}
+        {products.map((p, i) => (
+          <li key={i}>
+            {p.name} - €{p.price.toFixed(2)}
+            <button onClick={() => addToCart(p)}>
+              Aggiungi
+            </button>
           </li>
         ))}
       </ul>
+      <h2>Carrello</h2>
+      {addedProduct.length > 0 ? (
+        <ul>
+          {addedProduct.map((p, i) => (
+            <li key={i}>
+              {p.name} - €{p.price.toFixed(2)} x {p.quantity}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Il carrello è vuoto</p>
+      )}
     </>
   )
 }
